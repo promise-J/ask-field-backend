d6906242fb99fb12e4ab64ca55bdf658230768c8
Promise Johnson <chiemelapromise30@gmail.com>
2026-05-13 15:15:04 +0100

    update

--- src/middlewares/auth/auth.participant.middleware.ts
+++ src/middlewares/auth/auth.participant.middleware.ts
@@ -5,12 +5,12 @@ function participantAuth(req: Request, res: Response, next: NextFunction){
     try {
         authMiddleware(req, res, ()=> {
             if(req.user?.userType !== "researcher"){
-                return res.status(403).json({ message: "Forbidden! Researcher's Only" });
+                return res.status(403).json({ message: "Forbidden! Participant Only" });
             }
             next();
         })
     } catch (error) {
-        return res.status(403).json({ message: "Forbidden! Researcher's Only" });
+        return res.status(403).json({ message: "Forbidden! Participant Only" });
     }
 }
 
